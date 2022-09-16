91a16b60b41acb3e7380eba6e3b68efc833c7152
arewabolu <bolu.arewa@gmail.com>
2022-09-16 12:17:12 +0100

    handlers and method mechs

--- handles.go
+++ handles.go
@@ -1,103 +1,160 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strconv"
 	"strings"
-	"text/template"
-	"time"
 
 	"github.com/go-pg/pg/v10"
 )
 
-func homeHandle(w http.ResponseWriter, r *http.Request) {
+const maxUploadSize = 200 << 20
 
-}
-
-func loginHandle(w http.ResponseWriter, r *http.Request) {
-
-}
+//const maxUploadSize2 = 20 * 10 * 1024 * 1024
 
 func postCreateAccount(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	hashedPassword, err := passwordHash(r.PostFormValue("password"))
-	if err != nil {
-		fmt.Fprint(w, err)
+	creds := &User{}
+	jsErr := json.NewDecoder(r.Body).Decode(creds)
+	if jsErr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
-	creds := &User{
-		Email:        strings.ToLower(r.PostFormValue("email")),
-		PasswordHash: string(hashedPassword),
-		FullName:     r.PostFormValue("fname"),
-	}
-	fmt.Println(creds.Email)
-	queryErr := dbConn.Model(creds).Column("email").Where("?=?", pg.Ident("email"), creds.Email).Select()
+	fmt.Println(creds)
+
+	queryErr := dbConn.Model(creds).Column("email").Where("?=?", pg.Ident("email"), strings.ToLower(creds.Email)).Select()
 	if queryErr != nil {
-		fmt.Println("trying to create account")
-		fmt.Println(queryErr)
 		//convert names and email to lower case for db
-		_, insErr := dbConn.Model(creds).Insert()
-		//	err := dbMan.QueryRow(insertStatement, fullName, ), email)
+		hashedPaswd, _ := passwordHash(creds.PasswordHash)
+		dbCred := &User{
+			Email:        strings.ToLower(creds.Email),
+			FullName:     strings.ToLower(creds.FullName),
+			PasswordHash: string(hashedPaswd),
+		}
+
+		//Miracle in Cell No. 7, The Grand Heist
+
+		_, insErr := dbConn.Model(dbCred).Insert()
 		if insErr != nil {
-			fmt.Println("no error before here")
-			fmt.Println(insErr)
+			w.Write([]byte("unfortunately we couldn't create your account. Please try again."))
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		http.Redirect(w, r, "localhost:8080/login/", http.StatusPermanentRedirect)
+
+		SelectErr := dbConn.Model(dbCred).Column("id").Where("email=?", dbCred.Email).Select(&dbCred.ID)
+		if SelectErr != nil {
+			fmt.Println("post create error:", SelectErr)
+			return
+		}
+
+		err := createUserBucket(strAppender(strconv.Itoa(dbCred.ID)))
+		if err != nil {
+			fmt.Println("create bucket error:", err)
+		}
+		//	http.Redirect(w, r, "localhost:8080/login", http.StatusPermanentRedirect)
 		return
-
 	}
-	w.WriteHeader(http.StatusInternalServerError)
-}
-func createAccount(w http.ResponseWriter, r *http.Request) {
 
-	t, _ := template.ParseFiles("./assets/createAcc.html")
-	t.Execute(w, nil)
+	w.Write([]byte("account already exists. please login"))
+	w.WriteHeader(http.StatusOK)
 }
 
 func authHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+	creds := &User{}
+	json.NewDecoder(r.Body).Decode(creds)
 
-	email := strings.ToLower(r.PostFormValue("email"))
-	Password := r.PostFormValue("password")
+	email := strings.ToLower(creds.Email)
 	if email == "" {
-		w.WriteHeader(http.StatusExpectationFailed)
+		w.WriteHeader(http.StatusBadRequest)
 	}
+	Password := creds.PasswordHash
 	if Password == "" {
-		w.WriteHeader(http.StatusExpectationFailed)
+		w.WriteHeader(http.StatusBadRequest)
 	}
 
 	dbCred := &User{}
 	//scan into dbcred from database
-	SelectErr := dbConn.Model(dbCred).Column("passwordhash", "id").Where("email=?", email).Select(dbCred.PasswordHash, dbCred.ID)
+	SelectErr := dbConn.Model(dbCred).Column("passwordhash", "id").Where("email=?", email).Select(&dbCred.PasswordHash, &dbCred.ID)
 	if SelectErr != nil {
-		w.Write([]byte(SelectErr.Error()))
+		w.Write([]byte("please enter a valid email"))
 	}
 
-	pswdValErr := validateHash(dbCred.PasswordHash, Password)
+	pswdValErr := validateHash(Password, dbCred.PasswordHash)
 	if pswdValErr != nil {
-		w.Write([]byte(pswdValErr.Error()))
+		w.Write([]byte("incorrect password"))
 	}
-
-	JWTtoken, err := GenerateJWT(email)
+	JWTtoken, err := GenerateJWT(dbCred.ID)
 	if err != nil {
-		w.Write([]byte("User does not exist"))
-		http.Redirect(w, r, "localhost:8080/reg/", http.StatusPermanentRedirect)
+		w.Write([]byte("user does not exist")) //why here?
 	}
 
-	expirationTime := time.Now().Add(2 * time.Hour)
+	w.Header().Add("Authorization", "Bearer"+JWTtoken)
+	s := fmt.Sprintln("your user ID is "+strconv.Itoa(dbCred.ID), "please use this for subsequent request to upload or download")
+	w.Write([]byte(s))
 
-	http.SetCookie(w, &http.Cookie{
-		Name:    "jwt",
-		Value:   JWTtoken,
-		Expires: expirationTime,
-	})
-	fmt.Println(dbCred.ID, dbCred.FullName)
-	w.Header().Add("jwt", JWTtoken)
-
-	//http.Redirect(w, r, "localhost:8080/user/{dbCreds.ID}", http.StatusPermanentRedirect)
+	//	fmt.Println(dbCred.ID, dbCred.FullName)
 }
 
 func UserPageHandle(w http.ResponseWriter, r *http.Request) {
+	//get data from S3 to display as json response
 
+	list, err := listFiles(reqSplit(r))
+	if err != nil {
+		w.Write([]byte("failed to load list"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	jL, err := json.Marshal(list)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Write(jL)
+	w.WriteHeader(http.StatusOK)
+}
+
+func UserDownloadHandle(w http.ResponseWriter, r *http.Request) {
+	//get from s3 and return to user
+	w.Header().Set("Content-Type", "application/octet-stream")
+	file := new(Files)
+	json.NewDecoder(r.Body).Decode(file)
+	keyName := reqSplit(r) + "/" + file.Filename
+	usrFile := downloadFile(keyName)
+
+	byteFile, _ := io.ReadAll(usrFile)
+	w.Write(byteFile)
+
+}
+
+func UserUploadHandle(w http.ResponseWriter, r *http.Request) {
+	//parse data and send to s3 bucket
+
+	r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
+	err := r.ParseMultipartForm(maxUploadSize)
+
+	if err != nil {
+		if err == http.ErrNotMultipart {
+			w.Write([]byte("no file uploaded. Please confirm your upload"))
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		w.Write([]byte("the file uploaded is too large. Please select a file not larger than 200MB!"))
+		return
+	}
+
+	file, fileHeader, err := r.FormFile("upload-file")
+	if err == http.ErrMissingFile {
+		w.Write([]byte("unable to upload file. Please check upload info"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	err = uploadFile(reqSplit(r)+"/"+fileHeader.Filename, file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Write([]byte("yourfile has been sucessfully uploaded"))
+	w.WriteHeader(http.StatusOK)
 }
--- main.go
+++ main.go
@@ -8,24 +8,24 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+func groupRoutes(r chi.Router) {
+	r.Use(middlewareAUth)
+	r.Get("/user/{ID}/", UserPageHandle)
+	r.Get("/user/{ID}/download/", UserDownloadHandle)
+	r.Post("/user/{ID}/upload/", UserUploadHandle)
+}
+
 func main() {
 	router := chi.NewRouter()
+
 	router.Use(middleware.Logger)
-	router.Get("/", homeHandle)
-
-	router.Get("/login/", loginHandle)
-
-	router.Get("/user/*", UserPageHandle)
-	router.Get("/reg/", createAccount)
-	router.Post("/reg/", postCreateAccount)
+	router.Use(middleware.Recoverer)
+	router.Group(groupRoutes)
+	router.Post("/register/", postCreateAccount)
 	router.Post("/login/", authHandler)
-
-	//homeHandler := http.FileServer(http.Dir("./assets/root"))
-	//router.Handle("/", http.StripPrefix("/", homeHandler))
-	//router.HandleFunc("/", homeHandle)
-	//router.HandleFunc()
-	//router.HandleFunc()
-	//	router.HandleFunc("", homeHandle)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
+
+// Aim is to certify that the person sending a url
+// is the same one in the cookie id
